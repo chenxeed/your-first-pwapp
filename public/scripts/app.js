8203b74108e36d4654b47ed7b103d4de36c3c248
chenxeed <chenxeed@gmail.com>
2019-12-15 14:50:47 +0800

    feat: cache api load

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -163,7 +163,20 @@ function getForecastFromNetwork(coords) {
  */
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
-
+  if (window.caches) {
+    const url = `${window.location.origin}/forecast/${coords}`;
+    return caches.match(url)
+      .then((response) => {
+        if (response) {
+          return response.json();
+        }
+        return null;
+      })
+      .catch((err) => {
+        console.error('Error getting data from cache', err);
+        return null;
+      });
+  }
 }
 
 /**
@@ -198,6 +211,10 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
+    getForecastFromCache(location.geo)
+      .then(forecast => {
+        renderForecast(card, forecast);
+      })
 
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
